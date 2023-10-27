2d8e76829cdc893008d34b973f9643f824f77e06
ayfatoye <afatoye@tamu.edu>
2023-10-27 09:54:57 -0500

    made a change, testing to see if change propagates to vercel

--- pos/src/App.tsx
+++ pos/src/App.tsx
@@ -8,7 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          WELCOME TO FREAKING PIADA!!
+        </p>
+        <p>
+          Brought to you by R.A.T.S.
         </p>
         <a
           className="App-link"
