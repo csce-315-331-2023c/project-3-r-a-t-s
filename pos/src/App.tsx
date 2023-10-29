6af88db4b77e11b441ca9d92fbadbde04a17e04e
smsmeltzer <ssmeltzer@tamu.edu>
2023-10-29 17:26:45 -0500

    Added basic buttons to input Menu Items

--- pos/src/App.tsx
+++ pos/src/App.tsx
@@ -1,29 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Popup from 'reactjs-popup';
+import { useNavigate } from "react-router-dom";
+import 'reactjs-popup/dist/index.css';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  let order: string[] = [];
+  let curr_item = "";
+  let curr_size = "";
+  let curr_type = "";
+
+  const makeorderitem = (temp : number, item : string) => {
+    if (temp == 0) {
+      console.log("Choosing current item: ", item);
+      curr_item = item;
+    }
+    else if (temp == 1) {
+      console.log("Choosing a size: ", item);
+      curr_size = item;
+    }
+    else if (temp == 2) {
+      console.log("Choosing a type: ", item);
+      curr_type = item;
+    }
+  }
+
+  const addorderitem = () => {
+    order.push(curr_size + " " + curr_item + " " + curr_type);
+    console.log("Added new order item:", order.pop());
+
+  }
+
+  const addorder = () => {
+    console.log("Adding new Order");
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          WELCOME TO FREAKING PIADA!!
-        </p>
-        <p>
-          Brought to you by R.A.T.S.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>
+          PIADA 
+        </h1>
+        <p> Italian Street Food</p>
       </header>
+        <p> 
+            <Popup trigger=
+                {<button> Carbonara </button>}
+                position="bottom center">
+                <button onClick={() => makeorderitem(1, "SM")}> Small </button>
+                <button onClick={() => makeorderitem(1, "REG")}> Regular </button>
+                <br />
+                <button onClick={() => makeorderitem(2, "Penne")}> Penne </button>
+                <button onClick={() => makeorderitem(2, "Spaghetti")}> Spaghetti </button>
+                <br /> <br />
+                <button onClick={addorderitem}> Add {curr_item = "Carbonara"} to order </button>
+            </Popup>
+        </p>
+
+
+        <p>
+          <button> Build Your Own </button>
+        </p>
+
+
+          <h3>  Current Order:
+            <ul> {order} </ul>
+            <br />
+          <button onClick={addorder} > Pay </button>
+          </h3>
+        
     </div>
   );
 }
 
 export default App;
+
