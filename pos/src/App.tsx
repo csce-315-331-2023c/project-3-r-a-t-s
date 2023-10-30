e21406479353dc064d8ec7a3ead68a91711e31b9
smsmeltzer <ssmeltzer@tamu.edu>
2023-10-29 20:06:44 -0500

    Added buttons for all set menu items

--- pos/src/App.tsx
+++ pos/src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Popup from 'reactjs-popup';
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 import 'reactjs-popup/dist/index.css';
 
 import './App.css';
@@ -14,65 +14,145 @@ const App = () => {
   let curr_type = "";
 
   const makeorderitem = (temp : number, item : string) => {
-    if (temp == 0) {
+    if (temp === 0) {
       console.log("Choosing current item: ", item);
       curr_item = item;
     }
-    else if (temp == 1) {
+    else if (temp === 1) {
       console.log("Choosing a size: ", item);
       curr_size = item;
     }
-    else if (temp == 2) {
+    else if (temp === 2) {
       console.log("Choosing a type: ", item);
       curr_type = item;
     }
   }
 
   const addorderitem = () => {
-    order.push(curr_size + " " + curr_item + " " + curr_type);
+    order.push(curr_size + " " + curr_item + " " + curr_type);  
     console.log("Added new order item:", order.pop());
 
+    curr_size = "";
+    curr_item = "";
+    curr_type = "";
   }
 
   const addorder = () => {
     console.log("Adding new Order");
-
   }
 
   return (
     <div className="App">
       <header>
         <h1>
-          PIADA 
+        PIADA 
         </h1>
         <p> Italian Street Food</p>
       </header>
-        <p> 
-            <Popup trigger=
-                {<button> Carbonara </button>}
-                position="bottom center">
-                <button onClick={() => makeorderitem(1, "SM")}> Small </button>
-                <button onClick={() => makeorderitem(1, "REG")}> Regular </button>
-                <br />
-                <button onClick={() => makeorderitem(2, "Penne")}> Penne </button>
-                <button onClick={() => makeorderitem(2, "Spaghetti")}> Spaghetti </button>
-                <br /> <br />
-                <button onClick={addorderitem}> Add {curr_item = "Carbonara"} to order </button>
-            </Popup>
-        </p>
-
-
-        <p>
-          <button> Build Your Own </button>
-        </p>
-
-
-          <h3>  Current Order:
-            <ul> {order} </ul>
+      
+      <h3> Pasta: </h3>
+      <p> 
+        <Popup trigger=
+            {<button> Carbonara </button>}
+            position="bottom center" onOpen={() => makeorderitem(0, "Carbonara")}>
+            <button onClick={() => makeorderitem(1, "SM")}> Small </button>
+            <button onClick={() => makeorderitem(1, "REG")}> Regular </button>
             <br />
-          <button onClick={addorder} > Pay </button>
-          </h3>
-        
+            <button onClick={() => makeorderitem(2, "Penne")}> Penne </button>
+            <button onClick={() => makeorderitem(2, "Spaghetti")}> Spaghetti </button>
+            <br /> <br />
+            <button onClick={addorderitem}> Add to order </button>
+        </Popup>
+
+        <Popup trigger=
+            {<button> Diavolo </button>}
+            position="bottom center" onOpen={() => makeorderitem(0, "Diavolo")}>
+            <button onClick={() => makeorderitem(1, "SM")}> Small </button>
+            <button onClick={() => makeorderitem(1, "REG")}> Regular </button>
+            <br />
+            <button onClick={() => makeorderitem(2, "Penne")}> Penne </button>
+            <button onClick={() => makeorderitem(2, "Spaghetti")}> Spaghetti </button>
+            <br /> <br />
+            <button onClick={addorderitem}> Add to order </button>
+        </Popup>
+
+        <Popup trigger=
+            {<button> Basil Pesto </button>}
+            position="bottom center" onOpen={() => makeorderitem(0, "Basil Pesto")}>
+            <button onClick={() => makeorderitem(1, "SM")}> Small </button>
+            <button onClick={() => makeorderitem(1, "REG")}> Regular </button>
+            <br />
+            <button onClick={() => makeorderitem(2, "Penne")}> Penne </button>
+            <button onClick={() => makeorderitem(2, "Spaghetti")}> Spaghetti </button>
+            <br /> <br />
+            <button onClick={addorderitem}> Add to order </button>
+        </Popup>
+
+        <Popup trigger=
+            {<button> Marinara </button>}
+            position="bottom center" onOpen={() => makeorderitem(0, "Marinara")}>
+            <button onClick={() => makeorderitem(1, "SM")}> Small </button>
+            <button onClick={() => makeorderitem(1, "REG")}> Regular </button>
+            <br />
+            <button onClick={() => makeorderitem(2, "Penne")}> Penne </button>
+            <button onClick={() => makeorderitem(2, "Spaghetti")}> Spaghetti </button>
+            <br /> <br />
+            <button onClick={addorderitem}> Add to order </button>
+        </Popup>
+      </p>
+
+      <h3> Salad: </h3>
+      <p> 
+          <Popup trigger=
+              {<button> Deluxe Ceasar </button>}
+              position="bottom center" onOpen={() => makeorderitem(0, "Deluxe Ceasar Salad")}>
+              <button onClick={() => makeorderitem(1, "SM")}> Small </button>
+              <button onClick={() => makeorderitem(1, "REG")}> Regular </button>
+              <br /> <br />
+              <button onClick={addorderitem}> Add to order </button>
+          </Popup>
+
+          <Popup trigger=
+              {<button> Farmer's Market </button>}
+              position="bottom center" onOpen={() => makeorderitem(0, "Farmer's Market Salad")}>
+              <button onClick={() => makeorderitem(1, "SM")}> Small </button>
+              <button onClick={() => makeorderitem(1, "REG")}> Regular </button>
+              <br /> <br />
+              <button onClick={addorderitem}> Add to order </button>
+          </Popup>
+
+          <Popup trigger=
+              {<button> Avocado Chop </button>}
+              position="bottom center" onOpen={() => makeorderitem(0, "Avocado Chop Salad")}>
+              <button onClick={() => makeorderitem(1, "SM")}> Small </button>
+              <button onClick={() => makeorderitem(1, "REG")}> Regular </button>
+              <br /> <br />
+              <button onClick={addorderitem}> Add to order </button>
+          </Popup>
+
+          <Popup trigger=
+              {<button> Power Bowl </button>}
+              position="bottom center" onOpen={() => makeorderitem(0, "Power Bowl")}>
+              <button onClick={addorderitem}> Add to order </button>
+          </Popup>
+      </p>
+
+      <p>
+        <Popup trigger=
+            {<button> Click to open modal </button>} 
+            modal nested>
+            {
+                
+            }
+        </Popup>
+      </p>
+    
+      <h3>  Current Order:
+        <ul> {order} </ul>
+        <br />
+        <button onClick={addorder} > Pay </button>
+      </h3>
+
     </div>
   );
 }
