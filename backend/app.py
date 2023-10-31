9e81cceb5a8ac869da3496ae3f6d8ee1b29ffec5
ayfatoye <afatoye@tamu.edu>
2023-10-31 14:13:21 -0500

    testing now hosted backend application

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,23 @@
 import psycopg2
 import random
 from datetime import datetime, timedelta
+from flask import jsonify
+from flask import Flask
+app = Flask(__name__)
 
-DATABASE_URL = "dbname='csce315_904_01db' user='csce315_904_01user' host='csce-315-db.engr.tamu.edu' password='STAR'"
-conn = psycopg2.connect(DATABASE_URL)
-cur = conn.cursor()
 
-cur.execute(
-    "INSERT INTO orders (message_text) VALUES (%s);",
-    ("I think Im in love")
-)
+@app.route('/submit_order', methods=['POST'])
+def submit_order():
+    DATABASE_URL = "dbname='csce315_904_01db' user='csce315_904_01user' host='csce-315-db.engr.tamu.edu' password='STAR'"
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
 
-conn.commit()
-cur.close()
-conn.close()
+    cur.execute(
+        "INSERT INTO orders (message_text) VALUES (%s);",
+        ("I think Im in love")
+    )
+
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify({"message": "success"})
