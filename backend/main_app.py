6074d4f388062ad8a47aca4fb0513178b1b5b1ff
teresar510-tamu <teresar510@tamu.edu>
2023-11-08 21:07:10 -0600

    Added Blueprint to Divide Routes (Cashier/Manager)

---
+++ backend/main_app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from APIs.manager_backend import manager_BP
+from APIs.cashier_backend import cashier_BP
+
+app = Flask(__name__)
+
+# Register the blueprints
+app.register_blueprint(manager_BP, url_prefix="/api/manager")
+app.register_blueprint(cashier_BP, url_prefix="/api/cashier")
+
+if __name__ == '__main__':
+    #app.run()
+    app.run(debug=True)
