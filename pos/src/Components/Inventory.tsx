41c8216a81eb09579d42706f15e59dd57e8c4a82
ayfatoye <afatoye@tamu.edu>
2023-11-10 09:54:47 -0600

    finally have the inventory component rendering a table

--- pos/src/Components/Inventory.tsx
+++ pos/src/Components/Inventory.tsx
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+interface Item {
+  ingredient_id: number;
+  name: string;
+  quantity: number;
+  unit: string;
+}
+
 const InventoryComponent = () => {
-  const [inventoryData, setInventoryData] = useState(null);
+  const starterInventory: Item[] = [
+    { ingredient_id: 1, name: 'Item A', quantity: 10, unit: 'kg' },
+    { ingredient_id: 2, name: 'Item B', quantity: 5, unit: 'pieces' }
+  ];
+  const [inventoryData, setInventoryData] = useState<Item[]>(starterInventory);
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchInventory = async () => {
     setIsLoading(true);
     try {
       const response = await axios.get('/path-to-your-inventory-endpoint');
-      setInventoryData(response.data); // assuming the data is an array of inventory items
+      setInventoryData(response.data);
     } catch (error) {
       console.error('Failed to fetch inventory:', error);
     }
