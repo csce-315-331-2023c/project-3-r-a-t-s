599b9a08c71b7a0fd8115a0a0d122062cfba18c4
ayfatoye <afatoye@tamu.edu>
2023-11-12 13:45:24 -0600

    Manager Inventory view is fully operational. Note that for our deployed applications, we would need to update the backend applications to allow CORS from the vercel app, and in the frontend side of things, we need to update our requests to go to the hosted backend application.

--- backend/APIs/manager_backend.py
+++ backend/APIs/manager_backend.py
@@ -1,7 +1,51 @@
-from flask import Blueprint 
+from flask import Blueprint, jsonify, request
+from flask_cors import CORS
+import psycopg2 
 
 manager_BP = Blueprint('manager', __name__)
 
+# Enable CORS for all routes
+CORS(manager_BP)
+# CORS(manager_BP, origins = 'https://project-3-r-a-t-s.vercel.app')
+
+## Define database connection
+DB_PARAMS = {
+    'dbname': 'csce315_904_01db', 
+    'user': 'csce315_904_01user',
+    'password': 'STAR',
+    'host': 'csce-315-db.engr.tamu.edu',
+}
+
 @manager_BP.route('/get_order_history')
 def get_order_history():
-    return 'Manager Order History'
+    return jsonify({"message": "order history fetched"})
+
+
+@manager_BP.route('/get_inventory', methods=['GET'])
+def get_inventory():
+    try:
+        client_type = request.headers.get('X-Client-Type')
+        conn = psycopg2.connect(**DB_PARAMS)
+        cursor = conn.cursor()
+        cursor.execute('SELECT ingredient_id, name, quantity, unit FROM inventory')
+        inventory_items = cursor.fetchall()
+
+        items = [
+            {
+                "ingredient_id": item[0],
+                "name": item[1],
+                "quantity": item[2],
+                "unit": item[3]
+            }
+            for item in inventory_items
+        ]
+
+        cursor.close()
+        conn.close()
+
+        return jsonify({"items": items})
+
+    except Exception as e:
+        cursor.close()
+        conn.close()
+        return jsonify({"error": str(e)})
--- pos/src/Components/Inventory.tsx
+++ pos/src/Components/Inventory.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 interface Item {
   ingredient_id: number;
@@ -8,21 +8,71 @@ interface Item {
   unit: string;
 }
 
+const get_inventory = () => {
+  console.log("Paying for Order");
+  // console.table(order);
+  // setOrder([]);
+  // // Create an object with order data to send to the Flask API
+  // const orderData = {
+  //     items : order,
+  // };
+
+  // Set the Content-Type header to application/json
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Client-Type": "manager",
+    },
+  };
+  // Send a POST request to the Flask API
+  // Run locally with .post('http://127.0.0.1:5000/place_order', orderData)
+  axios.post("http://127.0.0.1:5000/api/manager/get_order_history");
+  //.post(`https://pos-backend-3c6o.onrender.com/api/cashier/place_order`, orderData, config)
+  // .then((response) => {
+  //     // Handle the response from the Flask API
+  //     console.log(response.data);
+  //     if (response.data.message === "Order placed successfully (From Backend)") {
+  //         // Order placed successfully
+  //         // console.log("Order placed :)");
+  //         order_total = response.data.order_total;
+  //         // TODO : Display confirmation message to user
+  //     } else {
+  //         // Handle Errors
+  //         console.error("Unexpected response:", response.data);
+  //     }
+  // })
+  // .catch((error : AxiosError) => {
+  //     console.error(error); // Handle errors, e.g., show an error message to the user.
+  // });
+};
+
 const InventoryComponent = () => {
   const starterInventory: Item[] = [
-    { ingredient_id: 1, name: 'Item A', quantity: 10, unit: 'kg' },
-    { ingredient_id: 2, name: 'Item B', quantity: 5, unit: 'pieces' }
+    { ingredient_id: 1, name: "Item A", quantity: 10, unit: "kg" },
+    { ingredient_id: 2, name: "Item B", quantity: 5, unit: "pieces" },
   ];
-  const [inventoryData, setInventoryData] = useState<Item[]>(starterInventory);
+  const [inventoryData, setInventoryData] = useState<Item[]>(() => []);
   const [isLoading, setIsLoading] = useState(false);
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Client-Type": "manager",
+    },
+  };
 
   const fetchInventory = async () => {
     setIsLoading(true);
     try {
-      const response = await axios.get('/path-to-your-inventory-endpoint');
-      setInventoryData(response.data);
+      const response = await axios
+        .get("http://127.0.0.1:5000/api/manager/get_inventory", config)
+        .then((response) => {
+          // Handle the response from the Flask API
+          console.log(response.data);
+          setInventoryData(response.data.items);
+        }) 
     } catch (error) {
-      console.error('Failed to fetch inventory:', error);
+      console.error("Failed to fetch inventory:", error);
     }
     setIsLoading(false);
   };
@@ -30,10 +80,11 @@ const InventoryComponent = () => {
   return (
     <div>
       <button onClick={fetchInventory} disabled={isLoading}>
-        {isLoading ? 'Loading...' : 'View current inventory'}
+        {isLoading ? "Loading..." : "View current inventory"}
+        {/* {inventoryData ? "Our Current Inventory:" : "View current inventory"} */}
       </button>
       {inventoryData && (
-        <table className='table table-striped w-100'>
+        <table className="table table-striped w-100">
           <thead>
             <tr>
               <th>Name</th>
