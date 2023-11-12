48f3689ded8338524eae416592b07912623f2a73
ayfatoye <afatoye@tamu.edu>
2023-11-12 13:45:47 -0600

    Manager Inventory view is fully operational. Note that for our deployed applications, we would need to update the backend applications to allow CORS from the vercel app, and in the frontend side of things, we need to update our requests to go to the hosted backend application.

--- pos/src/Components/Inventory.tsx
+++ pos/src/Components/Inventory.tsx
@@ -8,44 +8,6 @@ interface Item {
   unit: string;
 }
 
-const get_inventory = () => {
-  console.log("Paying for Order");
-  // console.table(order);
-  // setOrder([]);
-  // // Create an object with order data to send to the Flask API
-  // const orderData = {
-  //     items : order,
-  // };
-
-  // Set the Content-Type header to application/json
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-      "X-Client-Type": "manager",
-    },
-  };
-  // Send a POST request to the Flask API
-  // Run locally with .post('http://127.0.0.1:5000/place_order', orderData)
-  axios.post("http://127.0.0.1:5000/api/manager/get_order_history");
-  //.post(`https://pos-backend-3c6o.onrender.com/api/cashier/place_order`, orderData, config)
-  // .then((response) => {
-  //     // Handle the response from the Flask API
-  //     console.log(response.data);
-  //     if (response.data.message === "Order placed successfully (From Backend)") {
-  //         // Order placed successfully
-  //         // console.log("Order placed :)");
-  //         order_total = response.data.order_total;
-  //         // TODO : Display confirmation message to user
-  //     } else {
-  //         // Handle Errors
-  //         console.error("Unexpected response:", response.data);
-  //     }
-  // })
-  // .catch((error : AxiosError) => {
-  //     console.error(error); // Handle errors, e.g., show an error message to the user.
-  // });
-};
-
 const InventoryComponent = () => {
   const starterInventory: Item[] = [
     { ingredient_id: 1, name: "Item A", quantity: 10, unit: "kg" },
@@ -61,6 +23,7 @@ const InventoryComponent = () => {
     },
   };
 
+  //hosted backend: https://pos-backend-3c6o.onrender.com/api/cashier/place_order
   const fetchInventory = async () => {
     setIsLoading(true);
     try {
