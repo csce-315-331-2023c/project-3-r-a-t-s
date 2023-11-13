fba5951ba16154ed54698d8c1343d023b6dbeb82
ayfatoye <afatoye@tamu.edu>
2023-11-12 20:46:37 -0600

    View Menu is fully operational, for real, for real.

--- pos/src/Components/Menu.tsx
+++ pos/src/Components/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 interface MenuItem {
@@ -30,6 +30,10 @@ const MenuComponent = () => {
       setIsLoading(false);
     }
   };
+
+    // useEffect(() => {
+    //     fetchMenuItems();
+    // }, []);
 
   return (
     <div>
