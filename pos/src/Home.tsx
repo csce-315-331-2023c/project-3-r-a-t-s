c8c54678a7e1d14b9dffdefba18625240a301f80
smsmeltzer <ssmeltzer@tamu.edu>
2023-11-22 23:36:34 -0600

    Main page design finished

--- pos/src/Home.tsx
+++ pos/src/Home.tsx
@@ -1,20 +1,126 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import {MarkerF} from '@react-google-maps/api'
+
+import { BsFillTelephoneFill, BsFillMapFill } from "react-icons/bs";
+import { AiFillShop } from "react-icons/ai";
+import { MdShelves, MdDeliveryDining, MdTableRestaurant } from "react-icons/md";
+import { IoRestaurant } from "react-icons/io5";
 
 import './App.css';
+//import MenuBoardGUI from './MenuBoardGUI';
+
+const mapContainerStyle = {
+    width: '40vw',
+    height: '45vh',
+    display: 'table-cell'
+};
+
+const center = {
+    lat: 30.6242291,
+    lng: -96.3397041449957, 
+};
 
 const Home = () => {
     const navigate = useNavigate();
 
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyCXTwo6bsK6GtUP8sIAVSEnYuV44AeRFAg',
+      });
+    
+      if (loadError) {
+        return <div>Error loading maps</div>;
+      }
+    
+      if (!isLoaded) {
+        return <div>Loading maps</div>;
+      }
+
     return(
-        <div className="App">
-            <h3> Log in Here: </h3>
-            <button onClick={() => navigate('CashierGUI')}> Cashier GUI </button>
-            <h3> Click here if you're a customer: </h3>
-            <button onClick={() => navigate('CustomerGUI')}> Customer GUI </button>
-            <h3> Click here if you're a Manager: </h3>
-            <button onClick={() => navigate('ManagerGUI')}> Manager GUI </button>
+        <div className='home'>
+            <div className='home-top-bar'>
+                <h1> 
+                <img src="piada-icon.jpg" alt="Piada Icon of a Motor bike." className='icon'/> &nbsp;
+                   <b>Piada</b>  Italian Street Food 
+                <button onClick={() => navigate('ManagerGUI')} className='navigate-buttons'> Manager GUI</button>
+                <button onClick={() => navigate('CashierGUI')} className='navigate-buttons'> Cashier POS </button>
+                <button onClick={() => navigate('CustomerGUI')} className='navigate-buttons'> Customer Self-Service  </button>
+                </h1>
+            </div>
+
+            <div>
+            <div className='home-top-block'>
+                <h1 style={{
+                    fontSize: "150px"
+                }}> <b> College Station </b></h1>
+
+                <button onClick={() => navigate('MenuBoardGUI')}
+                style={{
+                    verticalAlign: "middle",
+                    textAlign: "center",
+                    borderRadius: "25px",
+                    border: "2px solid #FFFFFF",
+                    width: "250px",
+                    fontSize: "40px",
+                    marginTop: "-100px"
+                }} 
+                > View Menu </button> 
+
+            </div>
+
+            <div className='home-bottom-block'>
+            <div style={{
+                width: "30vw",
+                height: "40vh",
+                fontSize: "25px",
+                display: "table-cell",
+            }}> <br />
+                <BsFillMapFill/> &nbsp; 1025 University Dr., Suite 109, 
+                <br />College Station, TX 77845   <br /> <br />
+                <BsFillTelephoneFill /> &nbsp; 979.307.7611    <br />
+                <AiFillShop /> &nbsp; Curbside Pick-up    <br />
+                <MdShelves /> &nbsp; Grab & Go Shelves <br />
+                <IoRestaurant /> &nbsp; Dine In <br />
+                <MdDeliveryDining /> &nbsp; Third-Party Delivery <br />
+                <MdTableRestaurant /> &nbsp; Counter Pick-up
+            </div>
+
+            <div>
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={15}
+                center={center}>
+                <MarkerF position={center} />
+            </GoogleMap>
+            </div>
+
+            <div style={{display: "table-cell"}}>
+                <h3 style={{}}> <br /> 
+                 <u>Store Hours:</u> 
+                    <ul style={{
+                        listStyleType: "none",
+                        width: "30vw",
+                        height: "40vh"
+
+                        }}>
+                            <br />
+                        <li>Monday 10:45am-10:00pm</li>
+                        <li>Tuesday 10:45am-10:00pm</li>
+                        <li>Wednesday 10:45am-10:00pm</li>
+                        <li>Thursday 10:45am-10:00pm</li>
+                        <li>Friday 10:45am-10:00pm</li>
+                        <li>Saturday 10:45am-10:00pm</li>
+                        <li>Sunday 10:45am-10:00pm</li>
+                    </ul>
+                </h3>
+            </div>
+            </div>
+            </div>
+         
+
         </div>
+
     )
 }
 
---
+++ pos/src/MenuBoardGUI.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+import './MenuBoard.css'
+
+const MenuBoardGUI = () => {
+
+    return (
+        <div>
+            <p>Menu Board!</p>
+        </div>
+    )
+
+}
+export default MenuBoardGUI;
+
